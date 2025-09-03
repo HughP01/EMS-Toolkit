f57d31a5e518c5631f4279984c1cf6421c0b2969
HughP01 <71641914+HughP01@users.noreply.github.com>
2025-09-03 17:12:11 +0100

    Update __init__.py
    Added preprocessing

--- ems/__init__.py
+++ ems/__init__.py
@@ -1,1 +1,4 @@
-from .core import * #Most basic of functions and functions that dont really fit anywhere else
+from .core import * #Most basic of functions (e.g. Identifing null entries)
+from .preprocessing import * #Functions to do with preprocessing our data (e.g. transforming or removing outliers)
+#from .graphing import * #Graphing our df using seaborn and pyplot
+#other files go here...
