b06a8fb9637ba801c9d112e01c725adb1369ec08
HughP01 <71641914+HughP01@users.noreply.github.com>
2025-09-04 11:54:38 +0100

    Update __init__.py

--- ems/__init__.py
+++ ems/__init__.py
@@ -1,4 +1,5 @@
 from .core import * #Most basic of functions (e.g. Identifing null entries)
 from .preprocessing import * #Functions to do with preprocessing our data (e.g. transforming or removing outliers)
+from .basic_graphing import * #Graphing our df using basic plots for initial data exploration and analysis
 #from .graphing import * #Graphing our df using seaborn and pyplot
 #other files go here...
