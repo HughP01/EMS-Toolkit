0fd92c597c1a6a8d275e967fa48f9ae24f591106
HughP01 <71641914+HughP01@users.noreply.github.com>
2025-09-12 13:13:22 +0100

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,1 +1,6 @@
+import pandas as pd
 
+#Load data (many thanks to JehielT for hosting sample data)
+url = "https://raw.githubusercontent.com/JehielT/World-Happiness-Report/master/data.csv"
+df = pd.read_csv(url)
+df.head()
