7befe419b552ea36a98adcb718c27bc73a362adb
HughP01 <71641914+HughP01@users.noreply.github.com>
2025-12-01 19:18:18 +0000

    Add statistical libraries to stats.py
    Added imports for statistical functions and libraries.

---
+++ ems/stats.py
@@ -0,0 +1,6 @@
+#some stats functions thanks to core libraries such as numpy, scipy.stats, pandas, statsmodels
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
